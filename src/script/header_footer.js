530ff169c36933e17a054edc1730cca8f517a88b
pheithar <pheithar@hotmail.com>
2019-11-07 17:59:46 +0100

    Header v1

--- src/script/header_footer.js
+++ src/script/header_footer.js
@@ -1,17 +1,13 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
     document.getElementById("titleNav").style.display = "none";
-    document.getElementById("logoFenix").style.width = "0%";
-    document.getElementById("logoFenixShrink").style.width = "20%";
     document.getElementById("logoFenix").style.display = "none";
     document.getElementById("logoFenixShrink").style.display = "block";
   }
   else {
     document.getElementById("titleNav").style.display = "block";
-    document.getElementById("logoFenix").style.width = "10%";
-    document.getElementById("logoFenixShrink").style.width = "0%";
     document.getElementById("logoFenix").style.display = "block";
     document.getElementById("logoFenixShrink").style.display = "none";
   }
