b65a3096142325d7a4019a71c09d2bc4fbda6306
pheithar <pheithar@hotmail.com>
2019-11-22 18:43:05 +0100

    Todo con estilo correcto y generalizado

--- docs/script/crearNoticia.js
+++ docs/script/crearNoticia.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     $('#summernote').summernote();
+    console.log("A");
 });
 
 function prueba() {
   console.log($('#summernote').summernote('code'));
 
-  $('#formulario').append($('#summernote').summernote('code'))
+  $('.pruebas').append($('#summernote').summernote('code'))
 
 
 }
