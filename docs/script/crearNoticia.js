5640b8466e0c1103aab0aefbec23cfcc3ddd76fe
pheithar <pheithar@hotmail.com>
2019-11-30 16:24:50 +0100

    Empezar 'crear noticia'

--- docs/script/crearNoticia.js
+++ docs/script/crearNoticia.js
@@ -2,10 +2,23 @@ $(document).ready(function() {
     $('#summernote').summernote();
 });
 
-function prueba() {
+function crearNoticia() {
+
+  verificarNoticiaNueva();
+
+
   console.log($('#summernote').summernote('code'));
 
   $('.pruebas').append($('#summernote').summernote('code'))
 
+
+}
+
+
+//Comprueba que los campoes están rellenos
+function verificarNoticiaNueva() {
+  $(".noticiasFormulario").click(function(){
+
+  })
 
 }
