c0bf6a084009d5729eddde1cb48f614a86074df2
apmpm <alejandrpm@gmail.com>
2019-11-30 13:21:24 +0100

    Memoria V1.1 añadido confirmación eliminar noticia

--- docs/script/noticias.js
+++ docs/script/noticias.js
@@ -1,3 +1,8 @@
 function borrarNoticia(news) {
-  $(news).parents(".infoContenedor").remove();
+  if (confirm("¿Deseas borrar esta noticia?")){
+    //  title = $(news).parents(".infoContenedor").next(".titleContenedorNoticia").children(".tituloNoticia").text();
+
+    alert("Se ha borrado con éxito ")
+    $(news).parents(".infoContenedor").remove();
+  }
 }
