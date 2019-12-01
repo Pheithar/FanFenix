646316e3a2aebad3229b7900cb56cf5e83b1d6a7
apmpm <alejandrpm@gmail.com>
2019-12-01 19:02:09 +0100

    editar y borrar en todos lados

--- docs/script/noticias.js
+++ docs/script/noticias.js
@@ -2,7 +2,7 @@ function borrarNoticia(news) {
   if (confirm("¿Deseas borrar esta noticia?")){
     //  title = $(news).parents(".infoContenedor").next(".titleContenedorNoticia").children(".tituloNoticia").text();
 
-    alert("Se ha borrado con éxito ")
     $(news).parents(".infoContenedor").remove();
+    alert("Se ha borrado con éxito ")
   }
 }
